7feab90ad70e2ddfcce818a13509309fe99a99da
Prathamesh Paradkar <par76.pkar@gmail.com>
2025-09-01 12:24:57 +0530

    Dashboard, login, register

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './index.css'
-import { useAppDispatch } from './redux/store'
-import { useSelector } from 'react-redux'
-import { increment } from './redux/counter'
+
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Login from './Login/login'
+import Register from './Register/register'
+import Dashboard from './Dashboard/dashboard'
+import Layout from './components/layout'
+import Home from './Home/home'
 
 function App() {
-  const dispatch = useAppDispatch()
-  const counter = useSelector(state => state.counter)
-  // const [count, setCount] = useState(0)
 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => dispatch(increment())}>
-          count is {counter}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+    <Routes>
 
-      <button className="bg-blue-900 hover:bg-black-700 text-black py-2 px-2 rounded">
-       Click Me
-      </button>
-    </>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="/login" element={<Login />}></Route>
+    <Route path="/register" element={<Register />}></Route>
+    <Route path="/dashboard" 
+    element={
+      <Layout>
+        <Dashboard />
+      </Layout>
+      }>
+
+    </Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,54 @@
+// components/Sidebar.js
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  FolderIcon,
+  HomeIcon,
+  InboxIcon,
+  UsersIcon,
+  
+} from '@heroicons/react/outline'
+
+const navigation = [
+  { name: 'Accounts', href: '/account', icon: UsersIcon, current: true },
+  { name: 'Transaction', href: '/transaction', icon: Money, current: false },
+  { name: 'Bill Payment', href: '/bill', icon: FolderIcon, current: false },
+  { name: 'Deposit', href: '/deposit', icon: CalendarIcon, current: false },
+  { name: 'Help & Support', href: '/support', icon: InboxIcon, current: false },
+//   { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Sidebar() {
+  return (
+    <div className="flex flex-col flex-grow pt-5 bg-indigo-700 overflow-y-auto">
+      <div className="flex items-center flex-shrink-0 px-4">
+        <img
+          className="h-8 w-auto"
+          src="https://tailwindui.com/img/logos/workflow-logo-indigo-300-mark-white-text.svg"
+          alt="Workflow"
+        />
+      </div>
+      <div className="mt-5 flex-1 flex flex-col">
+        <nav className="flex-1 px-2 pb-4 space-y-1">
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className={classNames(
+                item.current ? 'bg-indigo-800 text-white' : 'text-indigo-100 hover:bg-indigo-600',
+                'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+              )}
+            >
+              <item.icon className="mr-3 flex-shrink-0 h-6 w-6 text-indigo-300" aria-hidden="true" />
+              {item.name}
+            </a>
+          ))}
+        </nav>
+      </div>
+    </div>
+  )
+}
